2770910339e6521952b1e37d85eb51102a694fc0
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2022-04-30 23:33:18 +1000

    ✨ feat(hook): add setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="kustomize-build",
+    description="Test kustomize build on kustomize files",
+    url="https://github.com/Truxnell/pre-commit",
+    version="0.0.1",
+    author="Nat Allan",
+    author_email="nat@natallan.com",
+    platforms="linux",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    packages=find_packages("."),
+    entry_points={
+        "console_scripts": [
+            "kustomize_build=hooks.kustomize_build:main",
+        ],
+    },
+)
